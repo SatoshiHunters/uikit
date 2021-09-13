408c0ccd0db35e1128426fb111d46c1d9648be60
Mohamed Doffer Mohamed Nasir <73598690+mohamed-nasir@users.noreply.github.com>
2021-09-13 18:15:43 +0800

    v0.0.1

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# uikit
+# 🥞 Satoshi UIkit
+
+[![Version](https://img.shields.io/npm/v/@satoshihunters/uikit)](https://www.npmjs.com/package/@satoshihunters/uikit) [![Size](https://img.shields.io/bundlephobia/min/@satoshihunters/uikit)](https://www.npmjs.com/package/@satoshihunters/uikit)
+
+Satoshi UIkit is a set of React components and hooks used to build pages on Satoshi's apps. It also contains a theme file for dark and light mode.
+
+## Install
+
+`yarn add @satoshihunters/uikit`
+
+## Setup
+
+### Theme
+
+Before using Satoshi UIkit, you need to provide the theme file to styled-component.
+
+```
+import { ThemeProvider } from 'styled-components'
+import { light, dark } from '@satoshihunters/uikit'
+...
+<ThemeProvider theme={isDark}>...</ThemeProvider>
+```
+
+### Reset
+
+A reset CSS is available as a global styled component.
+
+```
+import { ResetCSS } from '@satoshihunters/uikit'
+...
+<ResetCSS />
+```
---
+++ src/theme/colors.ts
@@ -0,0 +1,61 @@
+import { Colors } from "./types";
+
+export const baseColors = {
+  failure: "#ED4B9E",
+  primary: "#f98c08",
+  primaryBright: "#ffb948",
+  primaryDark: "#f98c08",
+  secondary: "#ffb948",
+  success: "#31D0AA",
+  warning: "#FFB237",
+  };
+  
+  export const brandColors = {
+    binance: "#F0B90B",
+  };
+  
+  export const lightColors: Colors = {
+    ...baseColors,
+    ...brandColors,
+    background: "#FFFFFF",
+    backgroundDisabled: "#E9EAEB",
+    contrast: "#261d13",
+    invertedContrast: "#FFFFFF",
+    input: "#f4efea",
+    // inputBorder: "#cccccc",
+    inputSecondary: "#cccccc",
+    tertiary: "#f5f3ef",
+    text: "#333333",
+    textDisabled: "#BDC2C4",
+    textSubtle: "#703c2c",
+    activeButton: "#397f49",
+    borderColor: "#E9EAEB",
+    card: "#FFFFFF",
+    nav: "#FFFFFF",
+    gradients: {
+      bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
+    },
+  };
+  
+  export const darkColors: Colors = {
+    ...baseColors,
+    ...brandColors,
+    background: "#353331",
+    backgroundDisabled: "#423d37",
+    contrast: "#FFFFFF",
+    invertedContrast: "#261e13",
+    input: "#5a4d3f",
+    // inputBorder: "#cccccc",    
+    inputSecondary: "#cccccc",
+    tertiary: "#473e35",
+    text: "#fff",
+    textDisabled: "#716a61",
+    textSubtle: "#fff",
+    activeButton: "#397f49",
+    borderColor: "#63584b",
+    card: "#2c2926",
+    nav: "#2c2926",
+    gradients: {
+      bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
+    },
+  };
